b5a4fdda8b0f0f2440bf8af713d4e78314f9e0d1
Mathieu Kerherve <mathieukerherve14@gmail.com>
2021-05-10 02:33:18 +0200

    added babel + css

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles/index.css"; 
+
+const test = {
+    "une": "une",
+    "tableau": [
+        1, 2, 3
+    ]
+}
+
+const test2 = {
+    ...test, 
+    "deux": 2,
+    "trois": 3
+}
+
+console.log(test)
+console.log(test2)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+let mode = "development"
+
+if (process.env.NODE_ENV === "production" ) {
+    mode = "production"
+}
+
+module.exports = {
+    mode,
+    
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, "css-loader"
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+
+    plugins: [new MiniCssExtractPlugin()],
+
+    devtool: "source-map",
+
+    devServer: {
+        contentBase: "./dist"
+    }
+}
